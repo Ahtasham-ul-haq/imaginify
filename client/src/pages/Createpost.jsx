9edc99ddf63d97a6194907a5360d0f0a88460e18
Ahtasham ul haq <ahtasham1802@gmail.com>
2024-01-21 19:49:09 +0500

    commit

---
+++ client/src/pages/Createpost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Createpost = () => {
+    return (
+        <div>Createpost</div>
+    )
+}
+
+export default Createpost
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home'
+import CreatePost from './Createpost'
+
+export {
+    Home,
+    CreatePost,
+}
